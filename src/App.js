77f415cd192ef3169da259792c9dcf3abb72c2dc
blur <thetobiterra@gmail.com>
2023-02-02 15:29:19 +0100

    comment

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import MovieSearch from './components/MovieSearch';
 import MovieList from './components/MovieList';
 
 function App() {
+  // This will contain all the movies in our app. It's our store
   const [movies, setMovies] = useState([])
 
+  // This is the functio responsible for creating our movie. How does it work? we
   const createMovie = (input) => {
     const updatedMovies = [...movies,
     { id: Math.round(Math.random() * 9999), title: input }]
